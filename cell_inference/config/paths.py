4a9c1c7547eff610692dcad534f99feaf0ae35ee
chenziao <chen8a803@gmail.com>
2022-09-14 16:00:42 -0500

    Update for Colab

--- cell_inference/config/paths.py
+++ cell_inference/config/paths.py
@@ -5,8 +5,10 @@ ROOT_DIR = os.path.sep.join(os.path.dirname(os.path.abspath(__file__)).split(os.
 RESOURCES_ROOT = os.path.join(ROOT_DIR, 'resources/')
 
 LIBRARY = 'nrnmech.dll' if platform.system() == 'Windows' else 'x86_64/.libs/libnrnmech.so'
-COMPILED_LIBRARY = os.path.join(RESOURCES_ROOT, 'compiled', 'mechanisms', LIBRARY)
-COMPILED_LIBRARY_REDUCED_ORDER = os.path.join(RESOURCES_ROOT, 'compiled', 'mechanisms_reduced_order', LIBRARY)
+COMPILED = os.path.join(RESOURCES_ROOT, 'compiled', 'mechanisms')
+COMPILED_REDUCED_ORDER = os.path.join(RESOURCES_ROOT, 'compiled', 'mechanisms_reduced_order')
+COMPILED_LIBRARY = os.path.join(COMPILED, LIBRARY)
+COMPILED_LIBRARY_REDUCED_ORDER = os.path.join(COMPILED_REDUCED_ORDER, LIBRARY)
 
 GEO_STANDARD = os.path.join(RESOURCES_ROOT, 'geom_standard.csv')
 GEO_STANDARD_AXON = os.path.join(RESOURCES_ROOT, 'geom_standard_axon.csv')
